ee6ea8413b7223a03ba29a309e89a836be34ec94
Kyle Zollo Venecek <kylezollo@gmail.com>
2021-12-21 16:42:23 -0400

    Add and document CUAHSI CVs

---
+++ R/data.R
@@ -0,0 +1,92 @@
+#' Allowed Data Types
+#'
+#' A data frame representation of the CUAHSI ODM 1.1 Controlled Vocabulary for Data Types
+#'
+#' @format A data frame with 16 rows and 2 variables:
+#' \describe{
+#'   \item{Term}{Controlled Vocabulary term}
+#'   \item{Definition}{Definition of Controlled Vocabulary term}
+#' }
+#' @source \url{http://his.cuahsi.org/mastercvreg/edit_cv11.aspx?tbl=DataTypeCV}
+"DataTypeCV"
+
+
+
+
+
+#' Allowed General Categories
+#'
+#' A data frame representation of the CUAHSI ODM 1.1 Controlled Vocabulary for General Category
+#'
+#' @format A data frame with 10 rows and 2 variables:
+#' \describe{
+#'   \item{Term}{Controlled Vocabulary term}
+#'   \item{Definition}{Definition of Controlled Vocabulary term}
+#' }
+#' @source \url{http://his.cuahsi.org/mastercvreg/edit_cv11.aspx?tbl=GeneralCategoryCV}
+"GeneralCategoryCV"
+
+
+
+
+#' Allowed Sample Media
+#'
+#' A data frame representation of the CUAHSI ODM 1.1 Controlled Vocabulary for Sample Medium
+#'
+#' @format A data frame with 24 rows and 2 variables:
+#' \describe{
+#'   \item{Term}{Controlled Vocabulary term}
+#'   \item{Definition}{Definition of Controlled Vocabulary term}
+#' }
+#' @source \url{http://his.cuahsi.org/mastercvreg/edit_cv11.aspx?tbl=SampleMediumCV}
+"SampleMediumCV"
+
+
+
+
+#' Allowed Units
+#'
+#' A data frame representation of the CUAHSI ODM 1.1 Controlled Vocabulary for Units
+#'
+#' @format A data frame with 394 rows and 4 variables:
+#' \describe{
+#'   \item{UnitsID}{Unique unit identifier}
+#'   \item{UnitsName}{Name of the unit}
+#'   \item{UnitsType}{Type of unit}
+#'   \item{UnitsAbbreviation}{Standardized abbreviation or symbol of the unit}
+#' }
+#' @source \url{http://his.cuahsi.org/mastercvreg/edit_cv11.aspx?tbl=Units}
+"UnitsCV"
+
+
+
+#' Allowed Value Types
+#'
+#' A data frame representation of the CUAHSI ODM 1.1 Controlled Vocabulary for Value Type
+#'
+#' @format A data frame with 7 rows and 2 variables:
+#' \describe{
+#'   \item{Term}{Controlled Vocabulary term}
+#'   \item{Definition}{Definition of Controlled Vocabulary term}
+#' }
+#' @source \url{http://his.cuahsi.org/mastercvreg/edit_cv11.aspx?tbl=ValueTypeCV}
+"ValueTypeCV"
+
+
+
+
+#' Allowed Variable Names
+#'
+#' A data frame representation of the CUAHSI ODM 1.1 Controlled Vocabulary for Variable Name
+#'
+#' @format A data frame with 925 rows and 2 variables:
+#' \describe{
+#'   \item{Term}{Controlled Vocabulary term}
+#'   \item{Definition}{Definition of Controlled Vocabulary term}
+#' }
+#' @source \url{http://his.cuahsi.org/mastercvreg/edit_cv11.aspx?tbl=VariableNameCV}
+"VariableNameCV"
+
+
+
+
