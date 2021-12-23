fb7779efd50cbfae990ef1189dff57c6d54a32d3
Kyle Zollo Venecek <kylezollo@gmail.com>
2021-12-23 10:39:00 -0400

    Handle incrementing methods

--- R/define_method.R
+++ R/define_method.R
@@ -8,16 +8,42 @@ define_method <- function(
 
   validate_arguments(fun.name = "define_method", fun.args = as.list(environment()))
 
+  # Assign the argument for "table" to a variable
+
+  flat_input <- get(table)
+
   # TODO what to do if local_variable and variable_code specified? Should only variable code be supported?
 
   # TODO this function will add a single column "MethodDescription_<MethodCode>"
   ## This is to prevent the (possibly already huge) flat table from
   ## doubling or tripling in size with the addition of multiple methods to a single/every variable
 
-  if (is.null(variable_code)) {
+  # Determine existing methods
+
+  existing_methods <- names(flat_input) %>%
+    stringr::str_detect('MethodDescription_') %>%
+    sum()
+
+  # TODO handle when there is methodLink
+
+  if (is.null(variable_code) & is.null(local_variable)) {
+
+    # Define new column names
+
+    method_sym <- rlang::sym(paste0("MethodDescription_", existing_methods + 1))
 
     # Add the method description and code to every observation
 
+    flat_output <- flat_input %>%
+      dplyr::mutate(!!method_sym := method_description)
+
+  } else if (is.null(variable_code) & !is.null(local_variable)) {
+
+    # TODO Create a single column table and join by local_variable
+
+  } else {
+
+    # TODO Create a single column table and join by variable_code
 
   }
 }
