f6ce26fe4c36473d45ad0faf22bcdbdfa9990a16
Kyle Zollo Venecek <kylezollo@gmail.com>
2021-12-23 10:50:51 -0400

    Add method to flat table by variable name

--- R/define_method.R
+++ R/define_method.R
@@ -24,13 +24,13 @@ define_method <- function(
     stringr::str_detect('MethodDescription_') %>%
     sum()
 
-  # TODO handle when there is methodLink
-
-  if (is.null(variable_code) & is.null(local_variable)) {
-
     # Define new column names
 
     method_sym <- rlang::sym(paste0("MethodDescription_", existing_methods + 1))
+
+  # TODO handle when there is methodLink
+
+  if (is.null(variable_code) & is.null(local_variable)) {
 
     # Add the method description and code to every observation
 
@@ -39,11 +39,25 @@ define_method <- function(
 
   } else if (is.null(variable_code) & !is.null(local_variable)) {
 
-    # TODO Create a single column table and join by local_variable
+    # Create method table
+
+    method_table <- dplyr::tibble(
+      local_variable_name = local_variable,
+      !!method_sym := method_description
+    )
+
+    # This is necessary to use setNames() in the by parameter of the join
+
+    lvn = "local_variable_name"
+
+    flat_output <- flat_input %>%
+      dplyr::left_join(method_table, by = setNames(lvn, local_variable_column))
 
   } else {
 
     # TODO Create a single column table and join by variable_code
 
   }
+
+  return(flat_output)
 }
--- tests/testthat/test-define_method.R
+++ tests/testthat/test-define_method.R
@@ -33,4 +33,78 @@ testthat::test_that("Single method is added (no variable, no method link)", {
 
   expect_true(all(expected_cols %in% names(res)))
 
+  expect_equal(length(unique(res$MethodDescription_1)), 1)
+
+
+})
+
+testthat::test_that("Single method is added to single variable (by variable name, no method link)", {
+
+  flat <<- data.frame(
+    "datetime" = as.Date('2021-12-21'),
+    "variable_name" = c('test_var', 'another_test_var'),
+    "value" = c(8.2693, 3.9628),
+    "unit" = c("degree celsius", "millimeter"),
+    "VariableCode" = c(1,2),
+    "VariableName" = NA_character_,
+    "VariableUnitsName" = NA_character_,
+    "SampleMedium" = NA_character_,
+    "ValueType" = NA_character_,
+    "IsRegular" = NA_character_,
+    "TimeSupport" = NA_character_,
+    "TimeUnitsName" = NA_character_,
+    "DataType" = NA_character_,
+    "GeneralCategory" = NA_character_,
+    "NoDataValue" = -9999)
+
+  on.exit(flat, add = TRUE)
+
+  res <- define_method(method_description = 'Here is the desciption of a test method.',
+                       local_variable = 'test_var')
+
+  expected_cols <- c(
+    "datetime", "variable_name", "value", "unit", "VariableCode", "VariableName",
+    "VariableUnitsName", "SampleMedium", "ValueType", "IsRegular", "TimeSupport",
+    "TimeUnitsName", "DataType", "GeneralCategory", "NoDataValue", "MethodDescription_1")
+
+  expect_true(all(expected_cols %in% names(res)))
+
+  expect_equal(length(unique(res$MethodDescription_1)), 2)
+
+})
+
+
+testthat::test_that("Single method is added to single variable (by variable code, no method link)", {
+
+  flat <<- data.frame(
+    "datetime" = as.Date('2021-12-21'),
+    "variable_name" = c('test_var', 'another_test_var'),
+    "value" = c(8.2693, 3.9628),
+    "unit" = c("degree celsius", "millimeter"),
+    "VariableCode" = c(1,2),
+    "VariableName" = NA_character_,
+    "VariableUnitsName" = NA_character_,
+    "SampleMedium" = NA_character_,
+    "ValueType" = NA_character_,
+    "IsRegular" = NA_character_,
+    "TimeSupport" = NA_character_,
+    "TimeUnitsName" = NA_character_,
+    "DataType" = NA_character_,
+    "GeneralCategory" = NA_character_,
+    "NoDataValue" = -9999)
+
+  on.exit(flat, add = TRUE)
+
+  res <- define_method(method_description = 'Here is the desciption of a test method.',
+                       local_variable = 'test_var')
+
+  expected_cols <- c(
+    "datetime", "variable_name", "value", "unit", "VariableCode", "VariableName",
+    "VariableUnitsName", "SampleMedium", "ValueType", "IsRegular", "TimeSupport",
+    "TimeUnitsName", "DataType", "GeneralCategory", "NoDataValue", "MethodDescription_1")
+
+  expect_true(all(expected_cols %in% names(res)))
+
+  expect_equal(length(unique(res$MethodDescription_1)), 2)
+
 })
