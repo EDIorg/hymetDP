7a267e3c53606ac49dedce1aeca529299bfb3c29
Kyle Zollo Venecek <kylezollo@gmail.com>
2022-01-13 20:05:20 -0400

    Outline the function

---
+++ R/create_series_catalog.R
@@ -0,0 +1,19 @@
+create_series_catalog <- function(
+  L0_flat = flat,
+  Sources = Sources,
+  Methods = Methods,
+  Variables = Variables,
+  Sites = Sites,
+  QualityControlLevels = QualityControlLevels,
+  DataValues = DataValues) {
+
+  validate_arguments(fun.name = "create_variables", fun.args = as.list(environment()))
+
+  # TODO automatically acquires the necessary columns from the mandatory tables
+
+  # TODO provide the list of columns each table should contain
+
+  # TODO calculate begin/end dates and do the ValueCount
+
+  # TODO coerce classes
+}
