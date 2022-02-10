1cf86f350a14aac043c7ae2c09895f599bf7c108
Kyle Zollo-Venecek <kylezollovenecek@Kyles-Mac-mini.local>
2022-02-10 11:00:52 -0400

    Add detect_os function

--- R/utilities.R
+++ R/utilities.R
@@ -756,3 +756,29 @@ get_eol <- function(path, file.name, os){
   eol
 
 }
+
+
+
+
+# Detect operating system
+#
+# @description
+#     This function uses \code{Sys.info} to detect the user's operating system
+#     and outputs an abbreviated character string to be used as inputs to OS
+#     specific function calls.
+#
+# @return
+#     \item{win}{Windows OS}
+#     \item{mac}{Mac OS}
+#
+detect_os <- function(){
+  sysinfo <- Sys.info()['sysname']
+  if (sysinfo == 'Darwin'){
+    os <- 'mac'
+  } else if (sysinfo == 'Windows'){
+    os <- 'win'
+  } else {
+    os <- 'lin'
+  }
+  os
+}
