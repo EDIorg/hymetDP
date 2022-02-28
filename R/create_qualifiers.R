e6ad4980860cb7e0ec1a6396867060e8aa48c409
Kyle Zollo-Venecek <kylezollovenecek@Kyles-Mac-mini.local>
2022-02-28 12:51:12 -0400

    Document create_qulifiers

--- R/create_qualifiers.R
+++ R/create_qualifiers.R
@@ -1,3 +1,17 @@
+#' Create the Qualifiers table
+#'
+#' @param L0_flat (tbl_df, tbl, data.frame) The fully joined source L0 dataset, in "flat" format (see details).
+#' @param QualifierCode (character) Column in \code{L0_flat} containing the code to indicate a given data qualifier.
+#' @param QualifierDescription (character) Column in \code{L0_flat} containing the text of the data qualifying comment, e.g., low battery voltage on sensor.
+#'
+#' @details This function appends columns to the \code{L0_flat} table and returns the augmented table.
+#'
+#' "flat" format refers to the fully joined source L0 dataset in "wide" form with the exception of the core observation variables, which are in "long" form (i.e. using the variable_name, value, unit columns of the observation table). This "flat" format is the "widest" an L1 hymetDP dataset can be consistently spread due to the frequent occurrence of L0 source datasets with > 1 core observation variable.
+#'
+#' @return
+#' @examples
+#' @export
+#'
 create_qualifiers <- function(
   L0_flat = flat,
   QualifierCode,
