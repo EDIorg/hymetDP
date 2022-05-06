9968f37535fee158834e0b34fa5a3c712da32ac6
Kyle <kylezollo@gmail.com>
2022-05-06 09:29:01 -0400

    Update prepare_summary_data

---
+++ R/search_data.R
@@ -0,0 +1,306 @@
+search_data <- function(text, variable_name, sample_medium, time_support,
+                        general_category, starts_before, ends_after,
+                        number_years, area, boolean = "AND") {
+
+  # Validate arguments --------------------------------------------------------
+
+  validate_arguments(
+    fun.name = "search_data",
+    fun.args = as.list(environment()))
+
+  ping_edi() # Warn if EDI is down
+
+  # Prepare summary data ------------------------------------------------------
+
+  # Download this object once per session and save to tempdir() for future calls
+  if ("hymetDP_search_index.rda" %in% dir(tempdir())) {
+    load(paste0(tempdir(), "/hymetDP_search_index.rda"))
+    d <- hymetDP_search_index
+  } else {
+    newrev <- suppressMessages(list_data_package_revisions("edi", "864", filter = "newest", env = 'staging'))
+    objurls <- suppressMessages(read_data_package(paste0("edi.864.", newrev), env = 'staging'))
+    objurls <- stringr::str_subset(objurls, "/data/")
+    objids <- stringr::str_extract(objurls, "(?<=/)[:alnum:]+$")
+    objnames <- suppressMessages(
+      lapply(objids, read_data_entity_name, packageId = paste0("edi.864.", newrev), env = 'staging'))
+    objnames <- unlist(objnames)
+    isdata <- !stringr::str_detect(objnames, "Function")
+    objurls <- objurls[isdata]
+    for (objurl in objurls) {
+      load(url(objurl))
+    }
+    hymetDP_search_index <- c(summary_data_edi)
+    save(hymetDP_search_index,
+         file = paste0(tempdir(), "/hymetDP_search_index.rda"),
+         version = 3)
+    d <- hymetDP_search_index
+  }
+
+  # Initialize an index of available datasets (use_i) for recording successful
+  # search hits, and an index of available sites within each dataset (sites_i)
+  # corresponding with taxa, num_taxa, and area search arguments.
+  # These are used later to collate the search results. use_i is initialized
+  # with logical because it will be used to select the matched datasets.
+  # sites_i is initialized with NA_character_ because they will be returned
+  # to the user in a data frame.
+
+  use_i <- rep(
+    list(
+      list(
+        text = F,
+        taxa = F,
+        num_taxa = F,
+        num_years = F,
+        sd_years = F,
+        area = F)),
+    length(d))
+  names(use_i) <- names(d)
+
+  sites_i <- rep(
+    list(
+      list(
+        text = NA_character_,
+        taxa = NA_character_,
+        num_taxa = NA_character_,
+        num_years = NA_character_,
+        sd_years = NA_character_,
+        area = NA_character_)),
+    length(d))
+  names(sites_i) <- names(d)
+
+  # Search --------------------------------------------------------------------
+  # Apply user specified search criteria to each dataset while recording
+  # successful hits.
+
+  for (i in seq_along(d)) {
+    arg_i <- rep(F, length(formals()))
+
+    # Search text
+
+    if (!missing(text)) {
+      if (boolean == "AND") {
+        use_i[[i]]$text <- all(
+          stringr::str_detect(
+            tolower(
+              paste(
+                d[[i]]$title,
+                d[[i]]$description,
+                d[[i]]$abstract,
+                collapse = ", ")),
+            tolower(text)))
+      } else if (boolean == "OR") {
+        use_i[[i]]$text <- stringr::str_detect(
+          tolower(
+            paste(
+              d[[i]]$title,
+              d[[i]]$description,
+              d[[i]]$abstract,
+              collapse = ", ")),
+          tolower(paste(text, collapse = "|")))
+      }
+      if (isTRUE(use_i[[i]]$text)) {
+        sites_i[[i]]$text <- names(d[[i]]$coordinates)
+      }
+    } else {
+      use_i[[i]]$text <- NULL
+      sites_i[[i]]$text <- NULL
+    }
+
+    # Search taxa
+    if (!missing(taxa)) {
+      taxa_i <- rep(F, length(d[[i]]$taxa))
+      for (k in 1:length(d[[i]]$taxa)) {
+        if (boolean == "AND") {
+          taxa_i[k] <- try(
+            all(
+              stringr::str_detect(
+                tolower(d[[i]]$taxa[[k]]$taxa),
+                tolower(taxa))),
+            silent = TRUE)
+          if (methods::is(taxa_i[k], "try-error")) {
+            # if (class(taxa_i[k]) == "try-error") {
+            taxa_i[k] <- FALSE
+          }
+        } else if (boolean == "OR") {
+          taxa_i[k] <- try(
+            stringr::str_detect(
+              tolower(d[[i]]$taxa[[k]]$taxa),
+              tolower(paste(taxa, collapse = "|"))),
+            silent = TRUE)
+          if (methods::is(taxa_i[k], "try-error")) {
+            taxa_i[k] <- FALSE
+          }
+        }
+      }
+      if (any(taxa_i, na.rm = T)) {
+        use_i[[i]]$taxa <- T
+        sites_i[[i]]$taxa <- names(d[[i]]$taxa)[taxa_i]
+      }
+    } else {
+      use_i[[i]]$taxa <- NULL
+      sites_i[[i]]$taxa <- NULL
+    }
+
+    if (!missing(num_taxa)) {
+      num_taxa_i <- rep(F, length(d[[i]]$taxa))
+      for (k in 1:length(d[[i]]$taxa)) {
+        num_taxa_i[k] <- (d[[i]]$taxa[[k]]$unique_taxa >= num_taxa[1]) &
+          (d[[i]]$taxa[[k]]$unique_taxa <= num_taxa[2])
+      }
+      if (any(num_taxa_i, na.rm = T)) {
+        use_i[[i]]$num_taxa <- T
+        sites_i[[i]]$num_taxa <- names(d[[i]]$taxa)[num_taxa_i]
+      }
+    } else {
+      use_i[[i]]$num_taxa <- NULL
+      sites_i[[i]]$num_taxa <- NULL
+    }
+
+    # Search temporal coverage
+
+    if (!missing(num_years)) {
+      years_i <- (unname(unlist(d[[i]]$number_of_years_sampled)) >= num_years[1]) &
+        (unname(unlist(d[[i]]$number_of_years_sampled)) <= num_years[2])
+      if (any(years_i, na.rm = T)) {
+        use_i[[i]]$num_years <- T
+        sites_i[[i]]$num_years <- names(d[[i]]$number_of_years_sampled)[years_i]
+      }
+    } else {
+      use_i[[i]]$num_years <- NULL
+      sites_i[[i]]$num_years <- NULL
+    }
+
+    if (!missing(sd_years)) {
+      sdyears_i <- (unname(unlist(d[[i]]$std_dev_interval_betw_years)) >= sd_years[1]) &
+        (unname(unlist(d[[i]]$std_dev_interval_betw_years)) <= sd_years[2])
+      if (any(sdyears_i, na.rm = T)) {
+        use_i[[i]]$sd_years <- T
+        sites_i[[i]]$sd_years <- names(d[[i]]$std_dev_interval_betw_years)[sdyears_i]
+      }
+    } else {
+      use_i[[i]]$sd_years <- NULL
+      sites_i[[i]]$sd_years <- NULL
+    }
+
+    # Search geographic coverage - Methods support point locations (location
+    # falls within the area defined by area) and areas (overlap
+    # between location area and the area defined by area).
+
+    if (!missing(area)) {
+      geographic_area_i <- rep(F, length(d[[i]]$coordinates))
+      for (k in 1:length(d[[i]]$coordinates)) {
+        if (length(unique(unlist(d[[i]]$coordinates[[k]]))) == 2) {
+          geographic_area_i[k] <-
+            (d[[i]]$coordinates[[k]]$N <= area[1]) &
+            (d[[i]]$coordinates[[k]]$S >= area[3]) &
+            (d[[i]]$coordinates[[k]]$E <= area[2]) &
+            (d[[i]]$coordinates[[k]]$W >= area[4])
+        } else if (length(unique(unlist(d[[i]]$coordinates[[k]]))) == 4) {
+          geographic_area_i[k] <-
+            (d[[i]]$coordinates[[k]]$N <= area[1]) & (d[[i]]$coordinates[[k]]$N >= area[3]) |
+            (d[[i]]$coordinates[[k]]$S <= area[1]) & (d[[i]]$coordinates[[k]]$S >= area[3]) |
+            (d[[i]]$coordinates[[k]]$W >= area[4]) & (d[[i]]$coordinates[[k]]$W <= area[2]) |
+            (d[[i]]$coordinates[[k]]$E >= area[4]) & (d[[i]]$coordinates[[k]]$E <= area[2])
+
+        }
+      }
+      if (any(geographic_area_i, na.rm = T)) {
+        use_i[[i]]$area <- T
+        sites_i[[i]]$area <- names(d[[i]]$coordinates)[geographic_area_i]
+      }
+    } else {
+      use_i[[i]]$area <- NULL
+      sites_i[[i]]$area <- NULL
+    }
+
+    # If no arguments are specified, then return the full list of sites for
+    # each dataset
+
+    if (missing(text) & missing(taxa) & missing(num_taxa) & missing(num_years) &
+        missing(sd_years) & missing(area)) {
+      sites_i <- lapply(
+        names(d),
+        function(x) {
+          list(all_sites = names(d[[x]]$coordinates))
+        })
+      names(sites_i) <- names(d)
+    }
+
+    # Indicate whether all search parameters were met
+    use_i[i] <- unlist(
+      lapply(
+        use_i[i],
+        function(x) {
+          all(unname(unlist(x)))
+        }))
+
+  }
+
+  # Return results ------------------------------------------------------------
+
+  d <- d[unname(unlist(use_i))]
+  sites_i <- sites_i[unname(unlist(use_i))]
+  output <- data.table::rbindlist(
+    lapply(
+      names(d),
+      function(x) {
+        # sites - EDI data are inconsistent in the representation of "sites"
+        # (in contrast to NEON) therefore these values are set to NA
+        if (d[[x]]$source == "EDI") {
+          sites <- NA_character_
+        } else {
+          sites <- paste(Reduce(intersect, sites_i[[x]]), collapse = ",")
+        }
+        # num_years - Report as a single value (EDI) or range (NEON)
+        if (length(d[[x]]$number_of_years_sampled) == 1) {
+          num_years <- unlist(d[[x]]$number_of_years_sampled)
+        } else {
+          num_years <- paste0(
+            "min = ", min(unlist(d[[x]]$number_of_years_sampled)), ", ",
+            "max = ", max(unlist(d[[x]]$number_of_years_sampled)))
+        }
+        # sampling_interval - Report as a single value (EDI) or range (NEON)
+        if (length(d[[x]]$std_dev_interval_betw_years) == 1) {
+          sampling_interval <- round(unlist(d[[x]]$std_dev_interval_betw_years), 2)
+        } else {
+          sampling_interval <- paste0(
+            "min = ", min(unlist(d[[x]]$std_dev_interval_betw_years)), ", ",
+            "max = ", max(unlist(d[[x]]$std_dev_interval_betw_years)))
+        }
+        # source_id
+        if (!is.null(d[[x]]$source_id)) {
+          source_id <- d[[x]]$source_id
+        } else {
+          source_id <- NA_character_
+        }
+        # source_id_url
+        if (!is.null(d[[x]]$source_id_url)) {
+          source_id_url <- d[[x]]$source_id_url
+        } else {
+          source_id_url <- NA_character_
+        }
+        # Return
+        list(
+          source = d[[x]]$source,
+          id = x,
+          title = d[[x]]$title,
+          description = d[[x]]$description,
+          abstract = d[[x]]$abstract,
+          years = num_years,
+          sampling_interval = sampling_interval,
+          sites = sites,
+          url = d[[x]]$url,
+          source_id = source_id,
+          source_id_url = source_id_url)
+      }))
+
+  if (nrow(output) == 0) {
+    return("No results found.")
+  } else {
+    # output <- dplyr::distinct(format_search_results(output))
+    output <- dplyr::distinct(output)
+    output <- tidyr::as_tibble(output)
+    return(output)
+  }
+
+}
--- R/utilities.R
+++ R/utilities.R
@@ -1152,3 +1152,158 @@ read_metadata <- function(packageId, env = "production") {
   httr::stop_for_status(resp, res)
   return(xml2::read_xml(res))
 }
+
+
+
+
+#' List data package revisions
+#'
+#' @param scope (character) Scope of data package
+#' @param identifier (numeric) Identifier of data package
+#' @param filter (character) Filter results by "newest" or "oldest"
+#' @param env (character) Repository environment. Can be: "production",
+#' "staging", or "development".
+#'
+#' @return (numeric) Revisions of a data package within a specified
+#' \code{scope} and \code{identifier}
+#'
+#' @family Listing
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # List revisions
+#' revisions <- list_data_package_revisions("knb-lter-arc", 20131)
+#' revisions
+#' #> [1] 1 2
+#' }
+list_data_package_revisions <- function(scope,
+                                        identifier,
+                                        filter = NULL,
+                                        env = "production") {
+  url <- paste0(
+    base_url(env), "/package/eml/",
+    paste(c(scope, as.character(identifier)), collapse = "/")
+  )
+  if (!is.null(filter)) {
+    url <- paste0(url, "?filter=", filter)
+  }
+  resp <- httr::GET(url, set_user_agent(), handle = httr::handle(""))
+  res <- httr::content(resp, as = "text", encoding = "UTF-8")
+  httr::stop_for_status(resp, res)
+  return(as.numeric(text2char(res)))
+}
+
+
+
+
+#' Read data package
+#'
+#' @param packageId (character) Data package identifier
+#' @param ore (logical) Return an OAI-ORE compliant resource map in RDF-XML
+#' format
+#' @param env (character) Repository environment. Can be: "production",
+#' "staging", or "development".
+#'
+#' @return (character or xml_document) A resource map with reference URLs to
+#' each of the metadata, data, and quality report resources that comprise the
+#' \code{packageId}.
+#'
+#' @family Accessing
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Get resource map
+#' resourceMap <- read_data_package(packageId = "knb-lter-cwt.5026.13")
+#' resourceMap
+#' #> [1] "https://pasta.lternet.edu/package/data/eml/knb-lter-cwt/5026/13/ ...
+#' #> [2] "https://pasta.lternet.edu/package/data/eml/knb-lter-cwt/5026/13/ ...
+#' #> [3] "https://pasta.lternet.edu/package/metadata/eml/knb-lter-cwt/5026 ...
+#' #> [4] "https://pasta.lternet.edu/package/report/eml/knb-lter-cwt/5026/1 ...
+#' #> [5] "https://pasta.lternet.edu/package/eml/knb-lter-cwt/5026/13"
+#'
+#' # Get resource map in ORE format
+#' resourceMap <- read_data_package(
+#'  packageId = "knb-lter-cwt.5026.13",
+#'  ore = TRUE
+#' )
+#' resourceMap
+#' #> {xml_document}
+#' #> <RDF xmlns:cito="http://purl.org/spar/cito/" xmlns:dc="http://purl.or ...
+#' #> [1] <rdf:Description rdf:about="https://pasta.lternet.edu/package/eml ...
+#' #> [2] <rdf:Description rdf:about="https://pasta.lternet.edu/package/eml ...
+#' #> [3] <rdf:Description rdf:about="https://pasta.lternet.edu/package/eml ...
+#' #> [4] <rdf:Description rdf:about="https://pasta.lternet.edu/package/eml ...
+#' #> [5] <rdf:Description rdf:about="https://pasta.lternet.edu/package/eml ...
+#' #> [6] <rdf:Description rdf:about="https://pasta.lternet.edu/package/eml ...
+#' #> [7] <rdf:Description rdf:about="http://environmentaldatainitiative.or ...
+#' #> [8] <rdf:Description rdf:about="http://www.openarchives.org/ore/terms ...
+#' #> [9] <rdf:Description rdf:about="http://www.openarchives.org/ore/terms ...
+#' }
+read_data_package <- function(packageId, ore = FALSE, env = "production") {
+  url <- paste0(
+    base_url(env), "/package/eml/",
+    paste(parse_packageId(packageId), collapse = "/")
+  )
+  if (ore) {
+    url <- paste0(url, "?ore")
+  }
+  resp <- httr::GET(url, set_user_agent(), handle = httr::handle(""))
+  res <- httr::content(resp, as = "text", encoding = "UTF-8")
+  httr::stop_for_status(resp, res)
+  if (ore) {
+    return(xml2::read_xml(res))
+  } else {
+    return(text2char(res))
+  }
+}
+
+
+
+
+#' Read data entity name
+#'
+#' @param packageId (character) Data package identifier
+#' @param entityId (character) Data entity identifier
+#' @param env (character) Repository environment. Can be: "production",
+#' "staging", or "development".
+#'
+#' @return (character) Name of \code{entityId} in \code{packageId}
+#'
+#' @family Accessing
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' # List entities
+#' entityIds <- list_data_entities(packageId = "knb-lter-cap.691.2")
+#' entityIds
+#' #> [1] "f6e4efd0b04aea3860724824ca05c5dd"
+#' #> [2] "d2263480e75cc7888b41928602cda4c6"
+#' #> [3] "d5cb83e4556408e48f636157e4dee49e"
+#'
+#' # Read name
+#' entityName <- read_data_entity_name(
+#'  packageId = "knb-lter-cap.691.2",
+#'  entityId = entityIds[1]
+#' )
+#' entityName
+#' #> [1] "691_arthropods_00742cd00ab0d3d02337e28d1c919654.csv"
+#' }
+read_data_entity_name <- function(packageId, entityId, env = "production") {
+  url <- paste0(
+    base_url(env), "/package/name/eml/",
+    paste(parse_packageId(packageId), collapse = "/"), "/",
+    entityId
+  )
+  resp <- httr::GET(url, set_user_agent(), handle = httr::handle(""))
+  res <- httr::content(resp, as = "text", encoding = "UTF-8")
+  httr::stop_for_status(resp, res)
+  return(text2char(res))
+}
