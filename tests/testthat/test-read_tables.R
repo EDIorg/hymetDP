fa3ddeb69300cb8c4d2d974198a3727b407edb9f
Kyle Zollo <kylezollo@gmail.com>
2022-05-16 13:25:04 -0400

    Merge pull request#5from kzollove/development (#13)

---
+++ tests/testthat/test-read_tables.R
@@ -0,0 +1,46 @@
+library(hymetDP)
+
+skip_on_cran()
+
+eml <- read_metadata('edi.1028.1')
+
+tables <- read_tables(
+  eml = eml,
+  strip.white = TRUE,
+  na.strings = "",
+  convert.missing.value = TRUE,
+  add.units = TRUE,
+  table.names = NULL)
+
+test_that("read_tables downloads a list of tables", {
+  expect_true(methods::is(tables, 'list'))
+})
+
+test_that("read_tables downloads all tables", {
+  expect_equal(length(tables), 3)
+})
+
+test_that("all list elements are tables", {
+  expect_true(all(unlist(lapply(tables,
+                                function(x) methods::is(x, 'data.frame')))))
+})
+
+test_that("units are correctly added", {
+
+  units <- stringr::str_detect(names(tables$WildflowerCES_FrenchBroadRiverBasin_site_info.csv), "unit_")
+
+  expect_equal(sum(units), 12)
+})
+
+test_that("only selected table is downloaded", {
+
+  tables <- read_tables(
+    eml = eml,
+    strip.white = TRUE,
+    na.strings = "",
+    convert.missing.value = TRUE,
+    add.units = TRUE,
+    table.names = "WildflowerCES_FrenchBroadRiverBasin_site_info.csv")
+
+  expect_equal(length(tables), 1)
+})
